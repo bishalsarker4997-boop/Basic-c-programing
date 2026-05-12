db5ad62138e1bf27590cbb99c106d5c3879f5191
Bishal Sarker <bishalsarker4997@gmail.com>
2026-05-12 12:16:41 +0600

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    if(op == '+')
+        printf("Result = %.2f", a + b);
+
+    else if(op == '-')
+        printf("Result = %.2f", a - b);
+
+    else if(op == '*')
+        printf("Result = %.2f", a * b);
+
+    else if(op == '/')
+        printf("Result = %.2f", a / b);
+
+    else
+        printf("Invalid Operator");
+
+    return 0;
+}
